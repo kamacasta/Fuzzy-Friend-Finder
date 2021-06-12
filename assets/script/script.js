4f5e20c8d320ffff71fdb6a7ae3ae2c3bb8d93ec
Thomas Begush <tbegush@gmail.com>
2021-06-11 19:21:14 -0700

    API calls

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,38 @@
+// petfinder info:
+// API Key
+// atfPgagnJHNfbVqbQA6zLRMWoYqUn6ojfbWtZq60qIpaK5rTjN
+// Secret
+// 97QbKuk6kztQyIuW8CjuCBEd8pN4sS83AdMYzmGmcopy
+var dogBreedList;
+var dogBreedImages;
 
-// this is the initial API call
-
-function getPetInfo() {
-
-    //searchTerm is our ID for the input
-    var searchTerm = document.querySelector("#searchTerm").value;
-    console.log(searchTerm);
-    fetch(
-      " PET FINDER API GOES HERE" +
-        searchTerm +
-        "QW1PdphVRcy3Pmf5qoWqfg91fEAk5o7yswNmRWPcsyyiADA14L"
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        petInfo = response;
-        console.log(petInfo);
-  
-        localStorage.setItem(searchTerm, JSON.stringify(response));
-
-
-
+//gets list of all dog breeds and turns it into an array
+function getDogBreedList() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var dogBreedList = response;
+      console.log(dogBreedList);
     });
-};
+for (const breed in dogBreedList) {
+    console.log(`${breed}: ${dogBreedList[breed]}`);
+}
+
+
+
+}
+
+function getDogBreedImages() {
+  fetch("https://dog.ceo/api/breed/" + dogBreed + "/images")
+  .then(function (response){
+      return response.json();
+  })
+  .then (function (response){
+      console.log(dogBreedImages);
+  })
+
+}
+
+getDogBreedList();
