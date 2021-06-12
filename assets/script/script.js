344d70f593a8c1ce0e059609c3f13af6e5200d4c
Thomas Begush <tbegush@gmail.com>
2021-06-12 16:57:26 -0700

    added random dog image

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,9 +6,18 @@
 var dogBreedList;
 var dogBreedImages;
 
-
-//random dog image
-
+//random dog image - need to set max size
+function getRandomDog() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (response){
+    console.log(response.messsage);
+    $("#randomdog").append('<img src="' + response.message + '">');
+    
+  })
+}
 //random cat image
 
 //gets list of all dog breeds and turns it into an array
@@ -20,25 +29,31 @@ function getDogBreedList() {
     .then(function (response) {
       var dogBreedList = response;
       console.log(dogBreedList);
-    })
-    for (const breed in dogBreedList) {
-    alert("Line 20!")
+    });
+  for (const breed in dogBreedList) {
+    alert("Line 20!");
     console.log(`${breed}: ${dogBreedList[breed]}`);
-}
-
-
-
+  }
 }
 
 function getDogBreedImages() {
   fetch("https://dog.ceo/api/breed/" + dogBreed + "/images")
-  .then(function (response){
+    .then(function (response) {
       return response.json();
-  })
-  .then (function (response){
+    })
+    .then(function (response) {
       console.log(dogBreedImages);
-  })
-
+    });
 }
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 33.4484, lng: -112.0740 },
+    zoom: 8,
+  });
+}
+
+getRandomDog()
 getDogBreedList();
