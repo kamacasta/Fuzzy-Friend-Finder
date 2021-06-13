c4aa051bea5cc2116c71a339e873ceebe9ce7bde
Thomas Begush <tbegush@gmail.com>
2021-06-12 18:25:02 -0700

    added random cats

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,12 +13,35 @@ function getRandomDog() {
     return response.json();
   })
   .then(function (response){
-    console.log(response.messsage);
     $("#randomdog").append('<img src="' + response.message + '">');
     
   })
 }
 //random cat image
+function getRandomCat() {
+  fetch("https://api.thecatapi.com/v1/images/search")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (response){
+    console.log(response.url);
+    $("#randomcat").append('<img src="' + response[0].url + '">');
+    
+  })
+}
+
+function getSpecificDogBreedImage () {
+  fetch("https://dog.ceo/api/breed/"+        +"/images/random")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (response){
+    console.log(response);
+ 
+  })
+}
+
+$("#dogButton").on("change", getSpecificDogBreedImage);
 
 //gets list of all dog breeds and turns it into an array
 function getDogBreedList() {
@@ -54,6 +77,6 @@ function initMap() {
     zoom: 8,
   });
 }
-
+getRandomCat()
 getRandomDog()
 getDogBreedList();
